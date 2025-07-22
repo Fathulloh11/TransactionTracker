1d290ffa0f54aa29de1b7fa08f6a33d930a966e3
Fathulloh_14 <fathullohrahmatov50@gmail.com>
2025-07-22 15:18:18 +0500

    Added TransactionDTOs

---
+++ MyExpenseTracker/DTOs/TransactionCreateDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MyExpenseTracker.Models;
+
+namespace MyExpenseTracker.DTOs;
+
+public class TransactionCreateDto
+{
+    [Required]
+    public DateTime Date { get; set; }
+
+    [Required]
+    public decimal Amount { get; set; }
+
+    [Required]
+    public TransactionType Type { get; set; }
+
+    [Required]
+    public Category Category { get; set; }
+
+    public string Note { get; set; }
+
+    [Required]
+    public int UserId { get; set; } 
+}
---
+++ MyExpenseTracker/DTOs/TransactionReadDto.cs
@@ -0,0 +1,14 @@
+﻿using MyExpenseTracker.Models;
+
+namespace MyExpenseTracker.DTOs;
+
+public class TransactionReadDto
+{
+    
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public TransactionType Type { get; set; }
+    public Category Category { get; set; }
+    public string Note { get; set; }
+}
